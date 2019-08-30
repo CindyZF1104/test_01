db729f6410e7b0d67b5d739d8d1c4c7ac074f58d
unknown <980670629@qq.com>
2019-08-30 10:35:45 +0800

    update runall

--- zentao.login/run_all.py
+++ zentao.login/run_all.py
@@ -5,7 +5,7 @@ rule = 'test*.py'
 discover = unittest.defaultTestLoader.discover(start_dir=casepath,pattern=rule)
 print(discover)
 
-reportpath = 'C:\\Users\\test\\PycharmProjects\\zentao.login\\report\\'+'report.html'
+reportpath = 'C:\\ctest\\GitHub\\zentao.login\\report\\'+'report.html'
 fp = open(reportpath,'wb')
 runner = HTMLTestRunner_cn.HTMLTestRunner(stream=fp,title='zentao登录报告',description='查看数据')
 runner.run(discover)
